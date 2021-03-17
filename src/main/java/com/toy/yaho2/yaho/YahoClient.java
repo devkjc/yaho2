28d3e4bb9faba8a0e2846ff5f53a3e68566f1a47
JongChan <kjc3590@songareeit.com>
2021-03-17 18:11:28 +0900

    오호라

--- src/main/java/com/toy/yaho2/yaho/YahoClient.java
+++ src/main/java/com/toy/yaho2/yaho/YahoClient.java
@@ -1,10 +1,12 @@
 package com.toy.yaho2.yaho;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(value = "yaho")
+@Component
 public interface YahoClient {
 
     @GetMapping("/test")
